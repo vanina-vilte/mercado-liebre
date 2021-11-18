cb200dc542202f2207212af56699bf17e2b0846a
vanina <vanina.vilte@hotmail.com>
2021-11-18 12:17:57 -0300

    mercado liebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log('Servidor funcionando')
+})
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/views/home.html');//cambia el path.resolve()
+})
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
