7ebfb02e48bdd7ad7b867610ff49650b5b0b347a
vanina <vanina.vilte@hotmail.com>
2021-11-23 11:13:23 -0300

    Procfile y cambios en json

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require ('express');
 const app = express();
-
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log('Servidor funcionando')
-})
+app.listen (process.env.PORT ||3000, ()=>{
+    console.log('Servidor funcionando bien');
+});
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/home.html');//cambia el path.resolve()
